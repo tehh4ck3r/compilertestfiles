6e93e88ba3a0fb0d43ee96325b714fb55cf16cf7
Kush Mahajani <kmahajani@scu.edu>
2018-02-13 19:13:26 -0800

    added smalltest.c

---
+++ smalltest.c
@@ -0,0 +1,43 @@
+int func(int a, int b) {
+    return 1;
+}
+
+int main(int argc, char** argv) {
+    int x;
+    int y;
+    char z;
+    char w;
+    int* a;
+    int f[10];
+
+    y * x;
+
+    x && y;
+
+    x == y;
+    x != y;
+    w == z;
+    w == x;
+
+    y / x;
+    y % x;
+    y < x;
+    y > x;
+    w <= z;
+    w >= x;
+
+    w % x / z;
+
+    x - 3;
+    y + 1;
+    y == "asdf";
+    y[13];
+    f[x];
+
+    -y;
+    !y;
+    sizeof y;
+    *a;
+    &y;
+    func(x, y);
+}
