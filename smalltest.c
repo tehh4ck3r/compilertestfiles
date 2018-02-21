1d3949982b55a6b704bce6e3e8c5ea6269a77add
Kush Mahajani <kmahajani@scu.edu>
2018-02-20 16:27:59 -0800

    Many updates

--- smalltest.c
+++ smalltest.c
@@ -1,4 +1,13 @@
 int func(int a, int b) {
+    int* z;
+    void *c;
+    char **d;
+    *a;
+    *z;
+    *c;
+    *d;
+    3[3][4];
+    d[1][2];
     return 1;
 }
 
@@ -8,11 +17,23 @@ int main(int argc, char** argv) {
     char z;
     char w;
     int* a;
+    int* lul;
     int f[10];
+    char* yolo;
+    void* _a;
+    char* _b;
+
+    x - a; /* should be error */
+    a + lul; /* should be error */
+
+    w && func;
 
     y * x;
 
+    z || y && x ||  y;
+
     x && y;
+    x || y;
 
     x == y;
     x != y;
@@ -27,17 +48,42 @@ int main(int argc, char** argv) {
     w >= x;
 
     w % x / z;
+    
+    _a != _b;
+    _a == x;
+    x == _a;
+
+    _a < _b;
+    _a < x;
+    x < z;
+
+    _a >= _b;
+    _a <= x;
+    x > z;
+    _a > x;
 
     x - 3;
     y + 1;
-    y == "asdf";
-    y[13];
+    yolo == "asdf";
+    f[13];
     f[x];
 
     -y;
     !y;
+    !_a;
+    !_b;
+    !a;
+    !w;
+    !f;
     sizeof y;
     *a;
     &y;
+    &10;
+
     func(x, y);
+    f && z;
+    -yolo;
+    -w;
+    -x;
+    -_a;
 }
